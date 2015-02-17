07a996d48243ef2759b6499ce80bfef8d93c398f
Mikael Karon <mikae@karon.se>
2015-02-17 12:03:18 +0800

    Don't modify `arguments`
    Spaces -> Tabs.

--- component.js
+++ component.js
@@ -1,6 +1,6 @@
 define([
 	"troopjs-core/component/emitter",
-  "troopjs-core/config",
+	"troopjs-core/config",
 	"./config",
 	"./executor",
 	"when/when"
@@ -15,12 +15,13 @@ define([
 	 */
 
 	var UNDEFINED;
+	var ARRAY_SLICE = Array.prototype.slice;
 	var NODE = com_config.node;
 	var PARENT = com_config.parent;
 	var COMPONENT = com_config.component;
-  var TYPE = core_config.emitter.type;
-  var EXECUTOR = core_config.emitter.executor;
-  var TARGET = "target";
+	var TYPE = core_config.emitter.type;
+	var EXECUTOR = core_config.emitter.executor;
+	var TARGET = "target";
 
 	return Component.extend(
 		/**
@@ -59,14 +60,14 @@ define([
 			 */
 			"trigger": function (type) {
 				var me = this;
-				var args = arguments;
+				var args = ARRAY_SLICE.call(arguments);
 				var bubble = true;
 
 				// Change first argument so we can use custom run logic
 				var event = args[0] = {};
-        event[TYPE] = type;
-        event[EXECUTOR] = executor;
-        event[TARGET] = me;
+				event[TYPE] = type;
+				event[EXECUTOR] = executor;
+				event[TARGET] = me;
 
 				return when.iterate(
 					function (node) {
@@ -91,13 +92,13 @@ define([
 			 */
 			"triggerHandler": function (type) {
 				var me = this;
-				var args = arguments;
+				var args = ARRAY_SLICE.call(arguments);
 
 				// Change first argument so we can use custom run logic
 				var event = args[0] = {};
-        event[TYPE] = type;
-        event[EXECUTOR] = executor;
-        event[TARGET] = me;
+				event[TYPE] = type;
+				event[EXECUTOR] = executor;
+				event[TARGET] = me;
 
 				return me.emit.apply(me, args);
 			}
