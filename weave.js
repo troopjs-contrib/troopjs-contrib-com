96df2d7d4da0ff418ffefd848cb788933fbc4a27
Mikael Karon <mikae@karon.se>
2015-03-11 17:06:39 +0800

    Cache required modules.

--- weave.js
+++ weave.js
@@ -8,6 +8,7 @@ define([
 	var TYPE = config.type;
 	var CHILDREN = config.children;
 	var COMPLETED = config.completed;
+	var MODULES = {};
 
 	function isRequireError(error) {
 		return error.hasOwnProperty("requireType");
@@ -26,11 +27,20 @@ define([
 		completed = completed === true || node.hasOwnProperty(COMPLETED);
 
 		return when
-			.promise(function (resolve, reject) {
-				// Use a local version of require to load the class. The node
-				// type has to either be a fully qualified module name or a map
-				// entry has to be added to the require configuration
-				localRequire([ node[TYPE] ], resolve, reject);
+			.promise(function (resolve) {
+				var module = node[TYPE];
+
+				// Resolve with ...
+				resolve(MODULES.hasOwnProperty(module)
+					// ... existing promise ...
+					? MODULES[module]
+					// ... new promise ...
+					: MODULES[module] = when.promise(function (_resolve, _reject) {
+						// Use a local version of require to load the class. The node
+						// type has to either be a fully qualified module name or a map
+						// entry has to be added to the require configuration
+						localRequire([ module ], _resolve, _reject);
+					}));
 			})
 			// Create instance
 			.then(function (Component) {
